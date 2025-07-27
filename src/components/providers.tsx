6e97547efd30f74d6f80c42cd938733b5e2bb947
ONEONUORA <monweebube44@gmail.com>
2025-07-27 19:24:43 +0100

    fix: bug fix

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,42 +1,42 @@
-"use client";
+// "use client";
 
-import { ReactNode } from "react";
-import { ThemeProvider } from "next-themes";
-import { sepolia } from "@starknet-react/chains";
-import {
-  StarknetConfig,
-  argent,
-  braavos,
-  useInjectedConnectors,
-  jsonRpcProvider,
-  voyager,
-} from "@starknet-react/core";
-import { Toaster } from "react-hot-toast";
-import { StarknetWalletProvider } from "./context/StarknetWalletContext";
+// import { ReactNode } from "react";
+// import { ThemeProvider } from "next-themes";
+// import { sepolia } from "@starknet-react/chains";
+// import {
+//   StarknetConfig,
+//   argent,
+//   braavos,
+//   useInjectedConnectors,
+//   jsonRpcProvider,
+//   voyager,
+// } from "@starknet-react/core";
+// import { Toaster } from "react-hot-toast";
+// import { StarknetWalletProvider } from "./context/StarknetWalletContext";
 
-export function Providers({ children }: { children: ReactNode }) {
-  const { connectors } = useInjectedConnectors({
-    recommended: [argent(), braavos()],
-    includeRecommended: "onlyIfNoConnectors",
-    order: "random",
-  });
+// export function Providers({ children }: { children: ReactNode }) {
+//   const { connectors } = useInjectedConnectors({
+//     recommended: [argent(), braavos()],
+//     includeRecommended: "onlyIfNoConnectors",
+//     order: "random",
+//   });
 
-  return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <StarknetConfig
-        chains={[sepolia]}
-        provider={jsonRpcProvider({
-          rpc: (chain) => ({ nodeUrl: process.env.NEXT_PUBLIC_RPC_URL }),
-        })}
-        connectors={connectors}
-        explorer={voyager}
-        autoConnect
-      >
-        <StarknetWalletProvider>
-          {children}
-          <Toaster position="bottom-right" />
-        </StarknetWalletProvider>
-      </StarknetConfig>
-    </ThemeProvider>
-  );
-}
+//   return (
+//     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+//       <StarknetConfig
+//         chains={[sepolia]}
+//         provider={jsonRpcProvider({
+//           rpc: (chain) => ({ nodeUrl: process.env.NEXT_PUBLIC_RPC_URL }),
+//         })}
+//         connectors={connectors}
+//         explorer={voyager}
+//         autoConnect
+//       >
+//         <StarknetWalletProvider>
+//           {children}
+//           <Toaster position="bottom-right" />
+//         </StarknetWalletProvider>
+//       </StarknetConfig>
+//     </ThemeProvider>
+//   );
+// }
